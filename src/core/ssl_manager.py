69c03a84033a8127dcdba28eb41e89774327637a
ashser004 <ashmithb796@gmail.com>
2026-06-18 03:34:03 +0530

    just ssl manager removed

--- src/core/ssl_manager.py
+++
@@ -1,3 +0,0 @@
-# This file is deprecated as TurboShare has switched to HTTP to avoid browser warnings.
-# The original code has been preserved in temp/ssl_manager.py as per your rules.
-# This placeholder prevents PyInstaller from analyzing and bundling the 'cryptography' library.
